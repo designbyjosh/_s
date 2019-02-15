0c496bd7fb0fef566f611f4f235fb5c91e378ed8
Josh Atkins <josh.atkins.dev@gmail.com>
2019-02-15 23:19:33 +0000

    copy dir

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+from shutil import copytree, ignore_patterns, rmtree
+import os
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
 
 while True:
     name = str(input("Enter name: "))
@@ -6,3 +10,11 @@ while True:
         break
     print("Must be aplhanumeric!")
 
+src = script_dir
+dst = script_dir + "/../" + name
+
+try:
+    rmtree(dst)
+    copytree(src, dst, ignore=ignore_patterns('[.]*', 'setup.py', '*.pyc', 'tmp*'))
+except FileExistsError:
+    print("Already exists!")
