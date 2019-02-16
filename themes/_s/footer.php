d2c33bb481cb356a4157e796b6cbad8bfc5ca7fd
Josh Atkins <josh.atkins.dev@gmail.com>
2019-02-16 11:39:44 +0000

    add basic layout to footer nav

--- themes/_s/footer.php
+++ themes/_s/footer.php
@@ -13,18 +13,26 @@
 
 			</div><!-- #content -->
 			<footer id="colophon" class="site-footer">
-				<div class="site-info">
+                <div class="site-footer-grid">
+    				<div class="site-info">
 
-				</div><!-- .site-info -->
+    				</div><!-- .site-info -->
 
-                <?php
-                    wp_nav_menu( array(
-                        'theme_location' => 'menu-2',
-                        'menu_id'        => 'footer-menu',
-                        'menu_class'     => 'menu simple',
-                        'container'=> null,
-                    ) );
-                ?>
+                    <div class="site-footer-nav">
+
+                        <div class="footer-menu-grid">
+                            <?php
+                                wp_nav_menu( array(
+                                    'theme_location'  => 'menu-2',
+                                    'menu_id'         => 'footer-menu',
+                                    'menu_class'      => 'menu simple',
+                                    'container'       => 'div',
+                                    'container_class' => 'footer-menu'
+                                ) );
+                            ?>
+                        </div>
+                    </div>
+                </div><!-- .site-footer-grid -->
 
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
