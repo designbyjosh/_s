d35ff2555ccc57fb1bef320c9aac6751d719601b
Josh Atkins <josh.atkins.dev@gmail.com>
2019-02-16 10:29:19 +0000

    simplify header

--- themes/_s/template-parts/content-page.php
+++ themes/_s/template-parts/content-page.php
@@ -9,23 +9,8 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+<article id="id-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<?php _s_post_thumbnail(); ?>
-
-	<div class="entry-content">
-		<?php
-		the_content();
-
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_s' ),
-			'after'  => '</div>',
-		) );
-		?>
-	</div><!-- .entry-content -->
 
 	<?php if ( get_edit_post_link() ) : ?>
 		<footer class="entry-footer">
@@ -49,4 +34,7 @@
 			?>
 		</footer><!-- .entry-footer -->
 	<?php endif; ?>
-</article><!-- #post-<?php the_ID(); ?> -->
+
+
+	
+</article><!-- #id-<?php the_ID(); ?> -->
